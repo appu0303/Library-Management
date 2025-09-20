c0101849e1b6304d12b94c4dcf3636eea1e5e497
= <AparnaKolape33@gmail.com>
2025-09-20 22:29:53 +0530

    Database connection with Liquibase has successfully created.

---
+++ Library-Management-System/src/main/java/com/library/Library_Management_System/dto/BookDTO.java
@@ -0,0 +1,11 @@
+package com.library.Library_Management_System.dto;
+
+public class BookDTO {
+    private Long id;
+    private String title;
+    private String author;
+    private String category;
+    private Integer availableCopies;
+    private Integer totalCopies;
+
+}
---
+++ Library-Management-System/src/main/java/com/library/Library_Management_System/entity/Book.java
@@ -0,0 +1,26 @@
+package com.library.Library_Management_System.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private String author;
+    private String category;
+    private int availableCopies;
+    private int totalCopies;
+
+}
